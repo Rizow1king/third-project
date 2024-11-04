b3733ec65bea38e3fe97a82ecbea9d61986296d4
muhammadrizo <mt936425155@gmail.com>
2024-11-04 14:32:44 +0500

    third commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# a = [(1, "Tohir", "Tohirov", "pyton")]
+# for user in a:
+#     print(user[0], user[1])
+# ----------------------------------------
+# from collections import namedtuple
+#
+# phones = (
+#     {"id": 1, "name": "Samsung Galaxy 1", "color": "red"},
+#     {"id": 2, "name": "Samsung Galaxy 2", "color": "blue"},
+#     {"id": 3, "name": "Samsung Galaxy 3", "color": "green"},
+#     {"id": 4, "name": "Samsung Galaxy 4", "color": "white"},
+#     {"id": 5, "name": "Samsung Galaxy 5", "color": "yellow"},
+# )
+#
+# Phone = namedtuple("phone", "id name color ")
+#
+# for phone in phones:
+#     p = Phone(**phone)
+#     print(p.id, p.name, p.color)
